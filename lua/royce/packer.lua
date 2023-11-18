40dfd4072d081a25ed3d987079419f807b7ba8c4
roycehoe <royceinbox@hotmail.com>
2023-11-18 23:09:24 +0800

    feat: Install lsp-zero

--- lua/royce/packer.lua
+++ lua/royce/packer.lua
@@ -35,4 +35,22 @@ return require('packer').startup(function(use)
   -- Fugitive: Git repo manager
   use('tpope/vim-fugitive')
 
+  -- LspZero: 
+  use {
+	  'VonHeikemen/lsp-zero.nvim',
+	  branch = 'v3.x',
+	  requires = {
+		  --- Uncomment these if you want to manage LSP servers from neovim
+		  -- {'williamboman/mason.nvim'},
+		  -- {'williamboman/mason-lspconfig.nvim'},
+
+		  -- LSP Support
+		  {'neovim/nvim-lspconfig'},
+		  -- Autocompletion
+		  {'hrsh7th/nvim-cmp'},
+		  {'hrsh7th/cmp-nvim-lsp'},
+		  {'L3MON4D3/LuaSnip'},
+	  }
+}
+
 end)
