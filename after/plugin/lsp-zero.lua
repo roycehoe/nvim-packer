49b9ba1361a287ed06135cdd6bd3be93d3f132c7
royce <royceinbox@hotmail.com>
2023-11-22 08:22:51 +0800

    feat: Enable tab completion

--- after/plugin/lsp-zero.lua
+++ after/plugin/lsp-zero.lua
@@ -3,12 +3,14 @@ local lsp_zero = require('lsp-zero')
 local cmp = require('cmp')
 
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
-
 local cmp_mappings = cmp.mapping.preset.insert({
   ['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
   ['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
   ['<cr>'] = cmp.mapping.confirm({ select = true }),
   ["<C-Space>"] = cmp.mapping.complete(),
+})
+cmp.setup({
+    mapping = cmp_mappings
 })
 
 lsp_zero.on_attach(function(client, bufnr)
